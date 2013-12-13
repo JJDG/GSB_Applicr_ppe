21c853a47849a0912876e70c71b49ca88ec2155d
josselin.d <josselin.damex@gmail.com>
2013-12-13 12:24:36 +0100

    Début de liste_fiche_c et bdd_get

--- application/models/bdd_get.php
+++ application/models/bdd_get.php
@@ -1,1 +1,20 @@
 <?php
+if (!defined('BASEPATH')){
+	exit('No direct script access allowed');
+}
+
+class Bdd_get extends CI_Model{
+	
+	function __construct(){
+		parent::__construct();
+		$this->load->database();
+	}
+	
+	//retourne un visiteur correspondant a l'id choisi
+	function getVisiteur(){
+		$query = $this->db->get_where($this->table, array('vis_matricule' => $id));
+		echo $query->row();
+		return $query->row();
+	}
+	
+}
