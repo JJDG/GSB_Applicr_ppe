79666531e696f6ea058af2a8e27dd2f91519630a
JohanGui <guicheron.johan@gmail.com>
2013-12-13 10:50:33 +0100

    database, connexion classe

--- application/controllers/connexion_c.php
+++ application/controllers/connexion_c.php
@@ -1,2 +1,16 @@
 <?php
- public
+if ( !defined('BASEPATH')) exit ('No direct script access allowed');
+
+class Connexion_c extends MY_Controller{
+	public function index(){
+		
+	}
+	
+	public function deconnexion(){
+		
+	}
+	
+	public function valider(){
+		
+	}
+}
