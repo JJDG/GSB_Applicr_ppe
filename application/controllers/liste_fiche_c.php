3068cc3a949eecf594b0d304189ee947c4b8210f
josselin.d <josselin.damex@gmail.com>
2013-12-13 11:55:40 +0100

    créer les controlleurs de base

---
+++ application/controllers/liste_fiche_c.php
@@ -0,0 +1,12 @@
+<?php
+if ( !defined('BASEPATH')) exit ('No direct script access allowed');
+
+class Liste_fiche_c extends MY_Controller{
+	
+	//affiche une liste de fiche du visiteur concerné a la date précisé
+	public function liste(){
+		
+		
+	}
+	
+}
---
+++ application/controllers/practicien_c.php
@@ -0,0 +1,6 @@
+<?php
+if ( !defined('BASEPATH')) exit ('No direct script access allowed');
+
+class Practicien_c extends MY_Controller{
+	
+}
