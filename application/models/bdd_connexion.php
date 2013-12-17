50467ece530be6cfd0dce1af189763767a1bd77b
JohanGui <guicheron.johan@gmail.com>
2013-12-17 10:22:09 +0100

    connexion

--- application/models/bdd_connexion.php
+++ application/models/bdd_connexion.php
@@ -8,13 +8,20 @@ class Bdd_connexion extends CI_Model{
 	public function sessionVisiteur($login, $mdp){
 		echo'<br/>ici';
 		$mdpV = $this->dateFormatMdp($mdp);
-		/*$req ="SELECT * FROM visiteur WHERE visnom =".$login." AND visdateembauche =".$mdpV;
-		
+		echo $mdpV;
+		$req ="SELECT * FROM visiteur WHERE vis_nom ='".$login."' AND vis_dateembauche =".$mdpV;
 		$query = $this->db->query($req);
 		foreach($query->result() as $row){
+			if($row == null){"mot de passe ou nom incorrect";}
+			else{
+				$id = $row->id;
+				$prenom = $row->prenom;
+				$nom = $row->nom;
+				$this->connecter($id,$nom,$prenom);
+			}
 		}
 		
-		return $row;*/
+		return $row;
 	}
 	
 	public function dateFormatMdp($mdp){
@@ -22,7 +29,23 @@ class Bdd_connexion extends CI_Model{
 		$jour = $mdp[0].$mdp[1];
 		$mois = $mdp[3].$mdp[4].$mdp[5];
 		$an = $mdp[7].$mdp[8].$mdp[9].$mdp[10];
+		$moisFin = 0;
+		$moisR="";
 		echo 'jour : '.$jour.' moi :'.$mois.' an : '.$an;
-		$lesMois = array('jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aog', 'sep', 'octobre', 'novembre', 'decembre');
+		$lesMois = array('jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aog', 'sep', 'oct', 'nov', 'dec');
+		for($i = 0; $i < 12; $i++){if($mois == $lesMois[$i]){$moisFin = $i+1;}}
+		if($moisFin < 10){$moisR = "0".$moisFin;}
+		return $an."-".$moisR."-".$jour;
+	}
+	
+	public function connecter($id,$nom,$prenom){
+		$newdata = array(
+	                   'id'  => $id,
+	                   'nom'     => $nom,
+				       'prenom'     => $prenom,
+	                   'logged_in' => TRUE
+	               );
+	
+		$this->session->set_userdata($newdata);
 	}
 }
