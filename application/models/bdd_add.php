d81960590006393e8e3652881d6c8b20aee10b01
josselin.d <josselin.damex@gmail.com>
2014-01-07 11:47:24 +0100

    test

--- application/models/bdd_add.php
+++ application/models/bdd_add.php
@@ -1,16 +1,4 @@
 <?php
-
-if (!defined('BASEPATH')){
-	exit('No direct script access allowed');
-}
-
 class Bdd_add extends CI_Model{
-	function __construct(){
-		parent::__construct();
-		$this->load->database();
-	}
 	
-	function addRapport(){
-		
-	}
 }
