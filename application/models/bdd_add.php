d4e6c1b4d86842e332895d5575eaf93d07755ae5
JohanGui <guicheron.johan@gmail.com>
2014-01-07 11:33:15 +0100

    Rapport formulaire et menu

--- application/models/bdd_add.php
+++ application/models/bdd_add.php
@@ -1,4 +1,16 @@
 <?php
+
+if (!defined('BASEPATH')){
+	exit('No direct script access allowed');
+}
+
 class Bdd_add extends CI_Model{
+	function __construct(){
+		parent::__construct();
+		$this->load->database();
+	}
 	
+	function addRapport(){
+		
+	}
 }
--- application/views/menuV.php
+++ application/views/menuV.php
@@ -4,16 +4,15 @@
     
         <h2>
     
-</h2>
-    
+		</h2>
       </div>
       <?php 
 	  echo "Connecté en tant que :";
 	  echo $this->session->userdata('nom')." ".$this->session->userdata('prenom').br(1);
 	  echo anchor('connexion_c/deconnexion', 'Déconnexion');
       $list = array(
-      		anchor('rapport_visite_c', 'Nouveaux'),
-      		anchor('liste_fiche_c', 'Consulter'),
+      		anchor('rapport_visite_c/creation', 'Nouveaux'),
+      		anchor('liste_fiche_c/liste', 'Consulter'),
       		anchor('medicament_c', 'Médicaments'),
       		anchor('praticien_c', 'Practiciens'),
       		anchor('visiteur_c', 'Autres visiteurs')
